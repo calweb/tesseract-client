f31f48f7a816f3783ddc27236f1083826d190736
Calvin Webster <calweb@gmail.com>
2015-10-28 14:14:59 -0400

    refactor to use electron boilerplate

--- app/app.js
+++ app/app.js
@@ -1,27 +1,32 @@
-(function () {
-  'use strict';
+(function () { 'use strict';
 
-  angular
-    .module('mtg-app', [
-      'ngRoute',
-      'mtg-app.cards'
-    ])
-    .config(function ($routeProvider) {
-      $routeProvider
-        .when('/', {
-          templateUrl: 'main.html',
-          controller: 'MainCtrl'
-        })
-        .when('/404', {
-          template: '<h1>Sorry, page not found.</h1>'
-        })
-        .otherwise({
-          redirectTo: '/404'
-        });
+  (function () {
+    'use strict';
 
-    })
-    .controller('MainCtrl', function ($scope) {
-      $scope.message = "Tesseract";
-    });
+    angular
+      .module('mtg-app', [
+        'ngRoute',
+        'mtg-app.cards'
+      ])
+      .config(function ($routeProvider) {
+        $routeProvider
+          .when('/', {
+            templateUrl: 'main.html',
+            controller: 'MainCtrl'
+          })
+          .when('/404', {
+            template: '<h1>Sorry, page not found.</h1>'
+          })
+          .otherwise({
+            redirectTo: '/404'
+          });
+
+      })
+      .controller('MainCtrl', function ($scope) {
+        $scope.message = "Tesseract";
+      });
+
+  })();
 
 })();
+//# sourceMappingURL=app.js.map
