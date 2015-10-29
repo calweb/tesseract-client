d7bbcb8ca6aa7f5e1550d3f6d0487c0dc5b0b638
Calvin Webster <calweb@gmail.com>
2015-10-29 13:44:49 -0400

    removed less added sass and moved stylesheets

--- tasks/build.js
+++ tasks/build.js
@@ -4,7 +4,6 @@ var pathUtil = require('path');
 var Q = require('q');
 var gulp = require('gulp');
 var rollup = require('rollup');
-var less = require('gulp-less');
 var sass = require('gulp-sass');
 var jetpack = require('fs-jetpack');
 
@@ -97,18 +96,12 @@ gulp.task('bundle', ['clean'], bundleTask);
 gulp.task('bundle-watch', bundleTask);
 
 
-var lessTask = function () {
-    return gulp.src('app/stylesheets/main.less')
-    .pipe(less())
-    .pipe(gulp.dest(destDir.path('stylesheets')));
-};
 var sassTask = function () {
     return gulp.src('app/stylesheets/styles.scss')
     .pipe(sass())
     .pipe(gulp.dest(destDir.path('stylesheets')));
 };
-gulp.task('less', ['clean'], lessTask);
-gulp.task('less-watch', lessTask);
+
 gulp.task('sass', ['clean'], sassTask);
 gulp.task('sass-watch', sassTask);
 
@@ -141,4 +134,4 @@ gulp.task('watch', function () {
 });
 
 
-gulp.task('build', ['bundle', 'less', 'sass', 'copy', 'finalize']);
+gulp.task('build', ['bundle', 'sass', 'copy', 'finalize']);
